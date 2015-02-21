6c0a6c7f6f34b0d6baf6582b400ef5af4c0e7c83
asbr90 <siebertartjom@googlemail.com>
2015-02-21 10:28:09 +0000

    fixed error

--- src/main.c
+++ src/main.c
@@ -16,10 +16,6 @@
 #include "ETRX357.h"
 #include "PhilipsHUE.h"
 
-void error(const char *msg) {
-	perror(msg);
-	exit(1);
-}
 
 int main(int argc, char *argv[]) {
 	int sockfd, newsockfd, portno;
@@ -51,36 +47,35 @@ int main(int argc, char *argv[]) {
 	}
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0)
-		error("ERROR opening socket");
-	
+		perror("ERROR opening socket");
+
 	bzero((char *) &serv_addr, sizeof(serv_addr));
 	portno = atoi(argv[1]);
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
 	serv_addr.sin_port = htons(portno);
 	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
-		error("ERROR on binding");
+		perror("ERROR on binding");
 	listen(sockfd, 5);
 	clilen = sizeof(cli_addr);
 
 	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
 
-			if (newsockfd < 0)
-				error("ERROR on accept");
-	while(1){
+	if (newsockfd < 0)
+		perror("ERROR on accept");
+	while (1) {
 		bzero(buffer, 256);
 		n = read(newsockfd, buffer, 255);
 		if (n < 0)
-			error("ERROR reading from socket");
+			perror("ERROR reading from socket");
 		printf("Server received message from client: %s\n", buffer);
 		n = write(newsockfd, "ACK", 4);
 
 		if (n < 0)
-			error("ERROR writing to socket");
+			perror("ERROR writing to socket");
 	}
 	close(newsockfd);
 	close(sockfd);
-
 
 	//	moveToLevel("0001","0B","0a",SEND_TO_GROUP);
 	//	changeONOFFState("0001","0B",ON_GROUP,SEND_TO_GROUP);
